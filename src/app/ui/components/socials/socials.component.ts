df51826af05e800fe04e28f8ab6337be8d37b538
amith <51911194+amith-patil@users.noreply.github.com>
2023-07-24 16:56:57 +0530

    updated theme and refactoring

--- src/app/ui/components/socials/socials.component.ts
+++ src/app/ui/components/socials/socials.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-socials',
   templateUrl: './socials.component.html',
-  styleUrls: ['./socials.component.scss']
+  styleUrls: ['./socials.component.scss'],
 })
 export class SocialsComponent {
-
+  _mSocials = [
+    {
+      logo: 'fab fa-twitter',
+      link: 'https://www.twitter.com',
+      delay: '100',
+    },
+    {
+      logo: 'fab fa-github',
+      link: 'https://www.github.com',
+      delay: '300',
+    },
+    {
+      logo: 'fa-brands fa-medium',
+      link: 'https://www.medium.com',
+      delay: '500',
+    },
+    {
+      logo: 'fa-brands fa-dribbble',
+      link: 'https://www.dribbble.com',
+      delay: '700',
+    },
+    {
+      logo: 'fab fa-linkedin',
+      link: 'https://www.linkedin.com',
+      delay: '1000',
+    },
+  ];
 }
--- src/app/ui/pages/home/home-career/home-career.component.ts
+++ src/app/ui/pages/home/home-career/home-career.component.ts
@@ -3,8 +3,83 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-career',
   templateUrl: './home-career.component.html',
-  styleUrls: ['./home-career.component.scss']
+  styleUrls: ['./home-career.component.scss'],
 })
 export class HomeCareerComponent {
+  // TODO: Update links
+  _mWorks = [
+    {
+      logo: 'assets/img/Dell_EMC_logo.png',
+      name: 'Dell EMC',
+      link: 'https://www.dell.com',
+    },
+    {
+      logo: 'assets/img/google_logo.png',
+      name: 'Google',
+      link: 'https://www.dell.com',
+    },
+    {
+      logo: 'assets/img/google_cloud_logo.png',
+      name: 'Google Cloud',
+      link: 'https://www.dell.com',
+    },
+    {
+      logo: 'assets/img/chronicle_logo.png',
+      name: 'Chronicle Security',
+      link: 'https://www.dell.com',
+    },
+  ];
 
+  _mSkills = [
+    {
+      logo: 'assets/img/typescript_logo.png',
+      name: 'TypeScript',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/angular_logo.png',
+      name: 'Angular',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/css_logo.png',
+      name: 'CSS',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/html_logo.png',
+      name: 'HTML',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/figma_logo.png',
+      name: 'Figma',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/tailwind_logo.png',
+      name: 'Tailwind',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/Bash_logo.png',
+      name: 'Bash',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/python_logo.png',
+      name: 'Python',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/go_logo.png',
+      name: 'Go',
+      link: 'https://www.typescriptlang.org/',
+    },
+    {
+      logo: 'assets/img/google_cloud_logo.png',
+      name: 'Google Cloud',
+      link: 'https://www.typescriptlang.org/',
+    },
+  ];
 }
