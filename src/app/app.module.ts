ca79e0e753f5575f63c50a3e4ae4bf86a644d591
amith <51911194+amith-patil@users.noreply.github.com>
2023-07-26 23:35:17 +0530

    updated career page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerModule } from './ui/container/container.module';
+import { FooterComponent } from './ui/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, ContainerModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/ui/pages/home/home-career/home-career.component.ts
+++ src/app/ui/pages/home/home-career/home-career.component.ts
@@ -6,6 +6,104 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-career.component.scss'],
 })
 export class HomeCareerComponent {
+  _mProfSkills = [
+    {
+      title: 'Protocols',
+      skills:
+        'HTTP, SMTP,\
+      SNMP, Restful APIs, ICAP, SSL,\
+      TLS, TCP/IP, UDP.',
+    },
+    {
+      title: 'RESEARCH',
+      skills:
+        'Log analysis and\
+      feature research, Customer\
+      Advisories, Engineering\
+      collaboration, Lab simulations\
+      and experimentation.',
+    },
+    {
+      title: 'TROUBLESHOOTING',
+      skills:
+        'Isilon\
+      Administration, InsightIQ,\
+      upgrades frameworks,\
+      enterprise level networking,\
+      Wireshark, Ktrace, Truss, iperf,\
+      Linux troubleshooting/\
+      administration.',
+    },
+    {
+      title: 'MANAGEMENT',
+      skills:
+        'Team player,\
+      backlog management, holistic\
+      approaches, results driven.',
+    },
+  ];
+  _mExperience = [
+    {
+      title: 'Technical Support Engineer I',
+      company_logo: 'assets/img/Dell_EMC_logo.png',
+      years: '2016-2018',
+      points: [
+        {
+          text: 'Assisting customers with high severity cases like data unavailability and access issues.',
+        },
+        {
+          text: 'working with Field engineers remotely during on-site troubleshooting',
+        },
+        {
+          text: 'Cluster networking, setting up DNS records and Isilon SmartConnect configurations, Microsoft Active Directory Management.',
+        },
+      ],
+    },
+    {
+      title: 'Senior Technical Support Engineer',
+      company_logo: 'assets/img/Dell_EMC_logo.png',
+      years: '2018-2021',
+      points: [
+        {
+          text: 'Level 2 engineer for the EMEA region for the administration related skills (Upgrades, HTTP, ICAP, SMTP, SNMP, Restful APIs, InsightIQ) with my secondary skill being Networking',
+        },
+        {
+          text: 'Working on escalations from L1 TSE and opening Jira Tickets with In-Market Engineering.',
+        },
+        {
+          text: 'Writing KB articles based on new bugs/issues found in the field (currently published 11 KB articles on the Dell Support site.)',
+        },
+        {
+          text: 'Providing inter departmental training programs to Legacy Dell Employees, along with new hires to the team.',
+        },
+        {
+          text: 'Lead a mentorship program to rapidly develop skills of new hires and global team mates.',
+        },
+      ],
+    },
+    {
+      title: 'Solutions Engineer',
+      company_logo: 'assets/img/google_logo.png',
+      years: '2021-Present',
+      points: [
+        {
+          text: 'Level 2 engineer for the EMEA region for the administration related skills (Upgrades, HTTP, ICAP, SMTP, SNMP, Restful APIs, InsightIQ) with my secondary skill being Networking',
+        },
+        {
+          text: 'Working on escalations from L1 TSE and opening Jira Tickets with In-Market Engineering.',
+        },
+        {
+          text: 'Writing KB articles based on new bugs/issues found in the field (currently published 11 KB articles on the Dell Support site.)',
+        },
+        {
+          text: 'Providing inter departmental training programs to Legacy Dell Employees, along with new hires to the team.',
+        },
+        {
+          text: 'Lead a mentorship program to rapidly develop skills of new hires and global team mates.',
+        },
+      ],
+    },
+  ];
   // TODO: Update links
   _mWorks = [
     {
