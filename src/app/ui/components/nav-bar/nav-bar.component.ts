2de2ca8867acfd54fba9aa44e4984ba167894e00
amith <51911194+amith-patil@users.noreply.github.com>
2023-08-18 23:00:27 +0530

    added dark mode

--- src/app/ui/components/nav-bar/nav-bar.component.ts
+++ src/app/ui/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  constructor(private el: ElementRef) {}
+  el1 = this.el.nativeElement.getElementsByClassName('icon');
 
+  onClick() {
+    if (document.documentElement.classList.contains('dark')) {
+      this.el1[0].classList.remove('fa-sun');
+      this.el1[0].classList.add('fa-moon');
+      document.documentElement.classList.remove('dark');
+    } else {
+      document.documentElement.classList.add('dark');
+      this.el1[0].classList.remove('fa-moon');
+      this.el1[0].classList.add('fa-sun');
+    }
+  }
 }
