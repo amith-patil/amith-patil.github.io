b640baab8b291f056e6161e95bcd1ace60fe2f25
amith-patil <amithsp93@live.com>
2024-08-20 22:33:18 +0530

    updated hero component and texts

--- src/app/ui/components/socials/socials.component.ts
+++ src/app/ui/components/socials/socials.component.ts
@@ -8,29 +8,12 @@ import { Component } from '@angular/core';
 export class SocialsComponent {
   _mSocials = [
     {
-      logo: 'fab fa-twitter',
-      link: 'https://www.twitter.com',
-      delay: '100',
-    },
-    {
       logo: 'fab fa-github',
-      link: 'https://www.github.com',
-      delay: '300',
-    },
-    {
-      logo: 'fa-brands fa-medium',
-      link: 'https://www.medium.com',
-      delay: '500',
-    },
-    {
-      logo: 'fa-brands fa-dribbble',
-      link: 'https://www.dribbble.com',
-      delay: '700',
+      link: 'https://www.github.com/amith-patil',
     },
     {
       logo: 'fab fa-linkedin',
-      link: 'https://www.linkedin.com',
-      delay: '1000',
+      link: 'https://www.linkedin.com/in/amithpatil/',
     },
   ];
 }
--- src/app/ui/pages/home/home-career/home-career.component.ts
+++ src/app/ui/pages/home/home-career/home-career.component.ts
@@ -71,20 +71,21 @@ export class HomeCareerComponent {
       years: '2021-Present',
       points: [
         {
-          text: 'Level 2 engineer for the EMEA region for the administration related skills (Upgrades, HTTP, ICAP, SMTP, SNMP, Restful APIs, InsightIQ) with my secondary skill being Networking',
+          text: 'Hired as a Level 3 engineer for the APAC/EMEA region for Google Chronicle. Worked primarily with Cybersecurity Clients with their Onboarding, Feature bugs and troubleshooting SIEM related issues.',
         },
         {
-          text: 'Working on escalations from L1 TSE and opening Jira Tickets with In-Market Engineering.',
+          text: 'Built internal tools to automate customer case analysis, Chat bots for notification of incoming and escalated cases as well as code fixes.Developed skills in writing Code in Go, Typescript,and Python. Upgraded my code readability in Java, Go, HTML,CSS Typescript and Protobufs.',
         },
         {
-          text: 'Writing KB articles based on new bugs/issues found in the field (currently published 11 KB articles on the Dell Support site.)',
+          text: 'Promoted to Level 4 Engineer in 2023, my role expanded to create tools to improve Engineer efficiency. This included automations and bots to improve Case Resolution time, assist Engineers in developing and creating playbooks for common issues.',
         },
         {
-          text: 'Providing inter departmental training programs to Legacy Dell Employees, along with new hires to the team.',
+          text: 'Manage a team of frontline Engineers, work predominantly on handling escalations, reducing response time and resolution time for cases, meeting with leadership teams from Engineering and Support to improve core engineering workloads for bugfixes.',
         },
         {
-          text: 'Lead a mentorship program to rapidly develop skills of new hires and global team mates.',
-        },
+          text: 'work with multiple Engineering teams on new feature launches, specifically on Designing supportability and Documentation of new features for a improved customer experience. Managed preview features feedbacks from Customers, Partner Engineering and Customer Engineering teams and help Product Management with their PRDs and GA feature launches.'
+          
+        }
       ],
     },
     {
